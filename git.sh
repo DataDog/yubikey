98858030c31545711d7dff4a94a09f498cf1c444
Trishank Karthik Kuppusamy <trishank.kuppusamy@datadoghq.com>
2021-01-21 01:55:26 +0800

    option to sign git commits&tags for select repos
    Signed-off-by: Trishank Karthik Kuppusamy <trishank.kuppusamy@datadoghq.com>

--- git.sh
+++ git.sh
@@ -6,21 +6,32 @@ set -e
 source env.sh
 source realname-and-email.sh
 
+# Determine whether to set globally or locally.
+if [[ -z "$1" ]]
+then
+    echo "Signing git commits & tags ${RED}${BOLD}GLOBALLY${RESET}"
+    SCOPE="--global"
+else
+    echo "Signing git commits & tags ${RED}${BOLD}LOCALLY${RESET}: $1"
+    SCOPE="--local"
+    cd "$1"
+fi
+
 # Set git name and email.
-echo "Setting your git-config global user.name..."
-$GIT config --global user.name "$REALNAME"
-echo "Setting your git-config global user.email..."
-$GIT config --global user.email "$EMAIL"
+echo "Setting your git-config user.name..."
+$GIT config $SCOPE user.name "$REALNAME"
+echo "Setting your git-config user.email..."
+$GIT config $SCOPE user.email "$EMAIL"
 
 # Ask user whether all git commits and tags should be signed.
 KEYID=$(get_keyid "$DEFAULT_GPG_HOMEDIR")
-echo "Setting git to use this GPG key globally."
+echo "Setting git to use this GPG key."
 echo "Also, turning on signing of all commits and tags by default."
 # Tell git to use this GPG key.
-$GIT config --global user.signingkey "$KEYID"
+$GIT config $SCOPE user.signingkey "$KEYID"
 # Also, turn on signing commits and tags by default.
-$GIT config --global commit.gpgsign true
-$GIT config --global tag.forceSignAnnotated true
+$GIT config $SCOPE commit.gpgsign true
+$GIT config $SCOPE tag.forceSignAnnotated true
 echo
 
 # Export GPG public key to GitHub.
