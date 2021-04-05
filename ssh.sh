79b1976b0d33c0228b3ec607457fc483ed4c99be
daisukixci <daisuki@tuxtrooper.com>
2021-04-05 22:38:35 +0200

    Add color to ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -59,17 +59,17 @@ configure_shell
 # Export SSH key derived from GPG authentication subkey.
 KEYID=$(get_keyid "$DEFAULT_GPG_HOMEDIR")
 SSH_PUBKEY=$KEYID.ssh.pub
-echo "Exporting your SSH public key to $SSH_PUBKEY"
+echo "${YELLOW}Exporting your SSH public key to ${SSH_PUBKEY}${RESET}"
 ssh-add -L | grep -iF 'cardno' > "$SSH_PUBKEY"
 echo "$SSH_PUBKEY" | $CLIP $CLIP_ARGS
 echo "It has also been copied to your clipboard."
-echo "You may now add it to GitHub: https://github.com/settings/ssh/new"
-echo "Opening GitHub..."
+echo "${YELLOW}You may now add it to GitHub: https://github.com/settings/ssh/new${RESET}"
+echo "${GREEN}Opening GitHub...${RESET}"
 $OPEN "https://github.com/settings/ssh/new"
-echo "Please save a copy in your password manager."
-read -rp "Have you done this? "
+echo "${YELLOW}Please save a copy in your password manager.${RESET}"
+read -rp "${YELLOW}Have you done this? ${RESET}"
 echo "Great."
 echo
-echo "You will need to ${RED}${BOLD}enter your PIN (once a day)${RESET}, and ${RED}${BOLD}touch your YubiKey everytime${RESET} in order to use SSH."
+echo "You will need to ${GREEN}${BOLD}enter your PIN (once a day)${RESET}, and ${GREEN}${BOLD}touch your YubiKey everytime${RESET} in order to use SSH."
 echo
 echo "Enjoy using your YubiKey at Datadog!"
