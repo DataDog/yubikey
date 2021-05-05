b9fd27ed2cfc1bca6403287c454e93106319c3a1
daisukixci <daisuki@tuxtrooper.com>
2021-05-05 12:13:50 +0200

    Only write Scdaemon conf if not empty and backup the conf

--- gpg.sh
+++ gpg.sh
@@ -109,6 +109,15 @@ fi
 set -e
 echo
 
+# Sometimes on macOS, a gpg update make the yubikey detection flaky or completely impossible
+# So we enforce the scdaemon.conf configuration to detect the Yubikey as it is on macOS only
+# cf env.sh
+# https://gpgtools.tenderapp.com/discussions/problems/58454-after-updating-to-gpgtools-20171-yubikey-no-longer-functions-properly-both-in-mail-gpg2-card-edit/page/1
+if [[ -n "$SCDAEMON_CONF" ]]; then
+    backup_conf "$DEFAULT_GPG_SCDAEMON_CONF"
+    echo -e "$SCDAEMON_CONF" > "$DEFAULT_GPG_SCDAEMON_CONF"
+fi
+
 # Show card information to user so they can be sure they are wiping right key
 # NOTE: explicitly check against default GPG homedir to make sure we are not wiping something critical...
 echo "YubiKey status:"
@@ -198,12 +207,6 @@ pinentry-program /usr/local/bin/pinentry-mac
 default-cache-ttl 86400
 max-cache-ttl 86400
 EOF
-
-# Sometimes on macOS, a gpg update make the yubikey detection flaky or completely impossible
-# So we enforce the scdaemon.conf configuration to detect the Yubikey as it is on macOS only
-# cf env.sh
-# https://gpgtools.tenderapp.com/discussions/problems/58454-after-updating-to-gpgtools-20171-yubikey-no-longer-functions-properly-both-in-mail-gpg2-card-edit/page/1
-echo -e "$SCDAEMON_CONF" > "$DEFAULT_GPG_SCDAEMON_CONF"
 
 # restart GPG daemons to pick up pinentry-mac
 $GPGCONF --kill all
