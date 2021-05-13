22a386da8bc0233a8e4b9784fb00a4f5330f5488
Trishank Karthik Kuppusamy <trishank.kuppusamy@datadoghq.com>
2021-05-13 10:45:33 -0400

    minor edit

--- notifications.sh
+++ notifications.sh
@@ -7,14 +7,14 @@
 # Honour git.sh setting or set to --global by default
 # Besides you can specify --local when directly called
 SCOPE=${1:---global}
-echo "Notifying the signature of git commits & tags ${GREEN}${BOLD}${SCOPE//--/}ly${RESET}"
+echo "Turning on notifications for signing git commits & tags ${GREEN}${BOLD}${SCOPE//--/}ly${RESET}"
 
 set -e
 
-echo "Deploying the notification script"
+echo "Deploying the notifications script"
 echo -e "$NOTIFICATION_CMD" > "$NOTIFICATION_SCRIPT_PATH"
 chmod u+x "$NOTIFICATION_SCRIPT_PATH"
-echo "${GREEN}The notification script has been deployed${RESET}"
+echo "${GREEN}The notifications script has been deployed${RESET}"
 
 $GIT config "$SCOPE" --add gpg.program "$NOTIFICATION_SCRIPT_PATH"
 echo "${GREEN}Notifications have been set up in git${RESET}"
