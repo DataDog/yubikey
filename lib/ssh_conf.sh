b969c287e7134f67c46561ab1135651f70509867
daisukixci <daisuki@tuxtrooper.com>
2022-01-04 17:49:48 +0100

    Use gpgconf to get the socket file

--- lib/ssh_conf.sh
+++ lib/ssh_conf.sh
@@ -19,9 +19,9 @@ function configure_shell {
     config_file_basename=$(basename "$config_file")
     echo "$config_file_basename detected"
     if [[ "$config_file_basename" == "config.fish" ]]; then
-        RC_SSH_CONF="set -gx SSH_AUTH_SOCK \${HOME}/.gnupg/S.gpg-agent.ssh"
+        RC_SSH_CONF="set -gx SSH_AUTH_SOCK \$($GPGCONF --list-dirs agent-ssh-socket)"
     else
-        RC_SSH_CONF="export SSH_AUTH_SOCK=\${HOME}/.gnupg/S.gpg-agent.ssh"
+        RC_SSH_CONF="export SSH_AUTH_SOCK=\$($GPGCONF --list-dirs agent-ssh-socket)"
     fi
     if ! grep -q "gpg-agent.ssh" "$config_file"; then
         echo "$RC_SSH_CONF" >> "$config_file"
