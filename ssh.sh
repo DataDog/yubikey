f76fb7e913ccc4f435f9877fc8885ca967bc6444
Tanner Prynn <tanner.prynn@datadoghq.com>
2022-07-18 10:02:23 -0500

    slightly more detailed doc in ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -6,8 +6,10 @@ set -e
 source env.sh
 
 # Sets gpg-agent for SSH_AUTH_SOCK in the user's profile & environment
-echo "${GREEN}Setting the GPG agent for SSH_AUTH_SOCK to handle SSH (and Git) actions${RESET}"
+echo "${GREEN}Setting the GPG agent for SSH_AUTH_SOCK to handle SSH actions${RESET}"
+echo "${YELLOW}This includes git actions like pushing and pulling from Github${RESET}"
 echo "${YELLOW}If this breaks your workflow, edit your profile to remove it${RESET}"
+echo "${YELLOW}And please open a ticket to let us know: https://github.com/DataDog/yubikey${RESET}"
 source lib/ssh_conf.sh
 
 # Export SSH key derived from GPG authentication subkey.
